60eb40dfbe94975c4aafa0fbe5e8c9970c2c6b67
Yoni Millen <yoni.millen@kbc.be>
2019-12-02 11:25:49 +0100

    Add ingredients to shoppinglist

--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/internal/shoppinglists/Shoppinglist.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/internal/shoppinglists/Shoppinglist.java
@@ -1,10 +1,13 @@
 package com.ezgroceries.shoppinglist.internal.shoppinglists;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class Shoppinglist {
     private UUID shoppingListId;
     private String name;
+    private List<String> ingredients;
 
     public Shoppinglist(UUID shoppingListId, String name) {
         this.shoppingListId = shoppingListId;
@@ -21,5 +24,20 @@ public class Shoppinglist {
 
     public String getName() {
         return name;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public void addIngredients(List<String> ingredients) {
+        if(this.ingredients == null) {
+            this.ingredients = new ArrayList<>();
+        }
+        this.ingredients.addAll(ingredients);
     }
 }
--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/internal/shoppinglists/ShoppinglistsController.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/internal/shoppinglists/ShoppinglistsController.java
@@ -1,6 +1,13 @@
 package com.ezgroceries.shoppinglist.internal.shoppinglists;
 
+import com.ezgroceries.shoppinglist.internal.cocktail.Cocktail;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,5 +22,66 @@ public class ShoppinglistsController {
     @ResponseStatus(HttpStatus.CREATED)
     public Shoppinglist createShoppinglist(@RequestBody Shoppinglist shoppinglist) {
         return shoppinglist;
+    }
+
+    @GetMapping
+    public List<Shoppinglist> getAllShoppinglists() {
+        List<Shoppinglist> shoppinglistList = mockShoppinglistList();
+
+        return shoppinglistList;
+    }
+
+    @PostMapping(path="{shoppinglistId}/cocktails")
+    public List<Cocktail> addCocktailToShoppinglist(@PathVariable(name="shoppinglistId")UUID shoppinglistId,
+            @RequestBody ArrayList<Cocktail> cocktailList) {
+        Shoppinglist shoppinglist = new Shoppinglist(shoppinglistId, "Dummy name");
+        cocktailList.forEach((cocktail) -> shoppinglist.addIngredients(cocktail.getIngredients()));
+
+        return cocktailList;
+    }
+
+    @GetMapping(path="{shoppinglistId}")
+    public Shoppinglist getShoppinglist(@PathVariable(name="shoppinglistId") UUID shoppinglistId) {
+        Shoppinglist shoppinglist = new Shoppinglist(shoppinglistId, "Dummy name");
+
+        List<Cocktail> cocktailList = mockCocktailList();
+
+        cocktailList.forEach((cocktail) -> shoppinglist.addIngredients(cocktail.getIngredients()));
+
+        return shoppinglist;
+    }
+
+    private List<Cocktail> mockCocktailList() {
+        List<Cocktail> cocktailList = new ArrayList<>();
+        cocktailList.add(new Cocktail(
+                UUID.fromString("23b3d85a-3928-41c0-a533-6538a71e17c4"), "Margerita",
+                "Cocktail glass",
+                "Rub the rim of the glass with the lime slice to make the salt stick to it. Take care to moisten..",
+                "https://www.thecocktaildb.com/images/media/drink/wpxpvu1439905379.jpg",
+                Arrays.asList("Tequila", "Triple sec", "Lime juice", "Salt"))
+        );
+        cocktailList.add(new Cocktail(
+                UUID.fromString("d615ec78-fe93-467b-8d26-5d26d8eab073"), "Blue Margerita",
+                "Cocktail glass",
+                "Rub rim of cocktail glass with lime juice. Dip rim in coarse salt..",
+                "https://www.thecocktaildb.com/images/media/drink/qtvvyq1439905913.jpg",
+                Arrays.asList("Tequila", "Blue Curacao", "Lime juice", "Salt"))
+        );
+        return cocktailList;
+    }
+
+    private List<Shoppinglist> mockShoppinglistList() {
+        List<Shoppinglist> shoppinglistList = new ArrayList<>();
+        List<Cocktail> cocktailList = mockCocktailList();
+
+        Shoppinglist shoppinglist1 = new Shoppinglist(UUID.randomUUID(), "Stephanie's birthday");
+        cocktailList.forEach((cocktail) -> shoppinglist1.addIngredients(cocktail.getIngredients()));
+        Shoppinglist shoppinglist2 = new Shoppinglist(UUID.randomUUID(), "My birthday");
+        cocktailList.forEach((cocktail) -> shoppinglist2.addIngredients(cocktail.getIngredients()));
+
+        shoppinglistList.add(shoppinglist1);
+        shoppinglistList.add(shoppinglist2);
+
+        return shoppinglistList;
     }
 }
